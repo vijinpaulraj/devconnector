7a3805f0cea088a8e33ce1b23191d349b7a583ca
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-03 23:41:45 +0100

    Implement client app

--- models/User.js
+++ models/User.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    requied: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    requied: true,
-  },
-  avatar: {
-    type: String,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        requied: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        requied: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
