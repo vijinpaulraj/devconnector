1f179d3631eea9d3cad5e7718543d88bb1f9c088
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-05 04:18:36 +0100

    Implement Auth for Login and Register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,25 +1,28 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: ''
     });
 
-    const { name, email, password, password2 } = formData;
+    const { email, password } = formData;
 
     const onChange = (e) =>
         setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        if (password !== password2) {
-            console.log('Passwords do not match');
-        } else {
-            console.log('SUCCESS');
-        }
+        login(email, password);
     };
+
+    if (isAuthenticated) {
+        return <Redirect to="/dahsboard" />;
+    }
 
     return (
         <Fragment>
@@ -64,4 +67,13 @@ const Login = () => {
     );
 };
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -24,7 +26,25 @@ export default function (state = initialState, action) {
                 loading: false
             };
 
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false
+            };
+
         case REGISTER_FAIL:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
+
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
