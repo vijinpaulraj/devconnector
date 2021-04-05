4692eeebe366d915ecf16bfcc20eb34456e6ffb3
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-05 01:09:43 +0100

    Implement Login and Register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-    return <div>Login</div>;
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log('SUCCESS');
+        }
+    };
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Sign Into Your Account
+            </p>
+            <form className="form" onSubmit={(e) => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        onChange={(e) => onChange(e)}
+                        value={email}
+                    />
+                    <small className="form-text">
+                        This site uses Gravatar so if you want a profile image,
+                        use a Gravatar email
+                    </small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        onChange={(e) => onChange(e)}
+                        value={password}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Register"
+                />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
+    );
 };
 
 export default Login;
