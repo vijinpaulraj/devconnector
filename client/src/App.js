f4337712d3cec3d0071ef6e2cc0a4fef0e9b4f92
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-05 23:24:19 +0100

    Implement create profile

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import { Landing } from './components/layout/Landing';
+import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
@@ -44,6 +45,11 @@ const App = () => {
                                 exact
                                 path="/dashboard"
                                 component={Dashboard}
+                            />
+                            <PrivateRoute
+                                exact
+                                path="/create-profile"
+                                component={CreateProfile}
                             />
                         </Switch>
                     </section>
