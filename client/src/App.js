6b05a8f0d5eb80422210c66c5d518fd15ab5c99f
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-06 00:22:50 +0100

    Implement Create/Edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile/CreateProfile';
+import EditProfile from './components/profile/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import { Provider } from 'react-redux';
@@ -50,6 +51,11 @@ const App = () => {
                                 exact
                                 path="/create-profile"
                                 component={CreateProfile}
+                            />
+                            <PrivateRoute
+                                exact
+                                path="/edit-profile"
+                                component={EditProfile}
                             />
                         </Switch>
                     </section>
