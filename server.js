80351a9185e26448957aff4059ac809f17bc4ab7
Vijin Paulraj <vijinpaulraj@gmail.com>
2021-04-12 00:43:35 +0100

    Update deafult config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,12 +8,19 @@ connectDB();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('API Running'));
-
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in prod
+if ((process.env.NODE_ENV = 'production')) {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
